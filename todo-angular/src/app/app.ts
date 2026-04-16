df102edc45b6958d75c1bff35550e463ad74f84f
zenbout-mohamed <mohamed.zenbout@laplateforme.io>
2026-04-16 16:23:42 +0200

    projet en cours de chantier

--- todo-angular/src/app/app.ts
+++ todo-angular/src/app/app.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { TodoListComponent } from './todo-list/todo-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [TodoListComponent],
+  template: `<app-todo-list />`
 })
-export class App {
-  protected readonly title = signal('todo-angular');
-}
+export class App {}
--- todo-angular/src/app/todo-list/todo-list.ts
+++ todo-angular/src/app/todo-list/todo-list.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
 interface Task {
   id: number;
   title: string;
@@ -10,29 +9,45 @@ interface Task {
   tags: string[];
 }
 
-
 @Component({
   selector: 'app-todo-list',
   standalone: true,
   imports: [FormsModule],
-  templateUrl: './todo-list.html', 
-  styleUrl: './todo-list.css',
+  templateUrl: './todo-list.html',
+  styleUrl: './todo-list.css'
 })
-export class TodoList {
-  tasks: string[] = [];
-  newTask = '';
+export class TodoListComponent {
 
-  addTask(){
-    if (!this.newTask.trim()) return; 
-    
-    this.tasks.push(this.newTask.trim());
-    this.newTask = '';
+  tasks: Task[] = [];
+  nextId = 1;
+
+  title = '';
+  author = '';
+  tags = '';
+
+  addTask() {
+    if (!this.title.trim() || !this.author.trim()) return;
+
+    this.tasks.push({
+      id: this.nextId++,
+      title: this.title.trim(),
+      author: this.author.trim(),
+      completed: false,
+      tags: this.tags
+        ? this.tags.split(',').map(t => t.trim())
+        : []
+    });
+
+    this.title = '';
+    this.author = '';
+    this.tags = '';
   }
 
-  deleteTask(index:number){  
-    this.tasks.splice(index , 1);
+  deleteTask(id: number) {
+    this.tasks = this.tasks.filter(t => t.id !== id);
   }
 
+  toggleTask(task: Task) {
+    task.completed = !task.completed;
+  }
 }
-
- 
